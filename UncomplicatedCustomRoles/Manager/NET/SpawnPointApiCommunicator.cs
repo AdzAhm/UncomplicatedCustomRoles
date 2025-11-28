b5f15340006f29eb792797c7b533a71bab68ce37
FoxWorn3365 <fede.cosma.ge@gmail.com>
2025-11-28 17:03:06 +0100

    updoot 2.0

--- UncomplicatedCustomRoles/Manager/NET/SpawnPointApiCommunicator.cs
+++ UncomplicatedCustomRoles/Manager/NET/SpawnPointApiCommunicator.cs
@@ -14,8 +14,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Threading.Tasks;
 using UncomplicatedCustomRoles.API.Enums;
 using UncomplicatedCustomRoles.API.Features;
